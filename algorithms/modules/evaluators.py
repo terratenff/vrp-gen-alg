c50e64b00f73a037dc92d3b48385897f0dd32d48
terratenff <49789537+terratenff@users.noreply.github.com>
2020-11-26 20:42:00 +0200

    Modified population individual implementation and initialized algorithm modules

---
+++ algorithms/modules/evaluators.py
@@ -0,0 +1,5 @@
+# if self.distance_time_ratio > 0:
+#     self.distance_to_time = lambda distance, ratio: distance / ratio
+# else:
+#     self.distance_time_ratio = self.distance_time_ratio * (-1)
+#     self.distance_to_time = lambda distance, ratio: distance * ratio
