d2f5decd78fbb30084b6ac79aadbb17fb07a83ed
terratenff <49789537+terratenff@users.noreply.github.com>
2020-11-30 21:04:30 +0200

    Implemented a timer

---
+++ algorithms/timer.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+"""
+timer.py:
+
+Implementation of a CPU timer that is used as a part of stopping criteria.
+"""
+
+import time
+
+
+def _time():
+    """
+    Convenience function that returns current process time as milliseconds.
+    """
+    return time.process_time() * 1000
+
+
+class Timer:
+    """
+    Captures CPU time and keeps track of it. In so doing,
+    various actions could be measured as milliseconds.
+    """
+
+    def __init__(self, goal=5000):
+        """
+        Timer constructor. It is stopped upon initialization.
+        :params goal: Optional threshold time (as milliseconds) that is used
+        as a time limit.
+        """
+
+        self.time_start = _time()
+        self.time_stop = _time()
+        self.time_goal = goal
+        self.running = False
+
+    def start(self):
+        """
+        Starts the timer.
+        """
+
+        self.time_start = _time()
+        self.time_stop = 0
+        self.running = True
+
+    def stop(self):
+        """
+        Stops the timer.
+        """
+
+        self.time_stop = _time() - self.time_start
+        self.running = False
+
+    def elapsed(self):
+        """
+        Calculates total time that has passed since starting the timer.
+        """
+
+        if self.running is False:
+            return _time() - self.time_start
+        else:
+            return (_time() - self.time_start) - self.time_stop
+
+    def difference(self):
+        """
+        Calculates time difference between time threshold and total elapsed time.
+        """
+
+        if self.running is False:
+            return self.time_goal - self.time_stop
+        else:
+            return self.time_goal - (_time() - self.time_start)
+
+    def past_goal(self):
+        """
+        Checks whether total elapsed time has exceeded the threshold.
+        """
+
+        return self.difference() < 0
